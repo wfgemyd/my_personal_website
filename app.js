a133d2d4e697300c2bb25aa8a36612c8225a3094
daniel shutov <66516254+wfgemyd@users.noreply.github.com>
2023-12-02 21:40:18 +0100

    main, about and projects are ready

---
+++ app.js
@@ -0,0 +1,84 @@
+// const hamburger = document.getElementById('hamburger');
+const nav = document.getElementById('nav');
+const headline = document.getElementById('headline');
+const button = document.getElementById('listen-button');
+
+hamburger.onclick = function () {
+    
+    this.classList.toggle('open');
+    nav.classList.toggle('show');
+    
+    if (window.getComputedStyle(headline).opacity == 1) { 
+        headline.style.opacity = 0;
+        button.style.opacity = 0;
+    } else {
+        headline.style.opacity = 1;
+        button.style.opacity = 1;
+    }
+      
+};
+
+const footer = document.querySelector('footer');
+const nav1 = document.getElementById('nav');
+
+
+function handleScroll() {
+  const footerRect = footer.getBoundingClientRect();
+  const navHeight = nav.offsetHeight;
+  
+  // Check if the footer is coming into the viewport
+  if (window.innerHeight - footerRect.top > 0) {
+    // Adjust the bottom position of the nav based on the footer's position
+    nav1.style.bottom = `${window.innerHeight - footerRect.top}px`;
+  } else {
+    // Reset the bottom position when the footer is not in view
+    nav1.style.bottom = '0';
+  }
+}
+window.addEventListener('scroll', handleScroll);
+
+
+
+const items = document.querySelectorAll('#grid > div');
+
+for (const item of items.values()) {
+    
+    new Waypoint.Inview({
+        element: item,
+        enter(direction) {
+            if (direction == 'down') {
+                item.classList.add('move-up');
+            }
+        },
+        exited(direction) {
+            if (direction == 'up') {
+                item.classList.remove('move-up');
+            }
+        }
+    });
+    
+}
+
+
+function calculateAge(birthDateString) {
+    var birthDate = new Date(birthDateString);
+    var today = new Date();
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
+
+document.getElementById('age').textContent = calculateAge('1997-05-20');
+
+
+
+
+
+
+
+
+
+
---
+++ projects.js
@@ -0,0 +1,108 @@
+const hamburger = document.getElementById('hamburger');
+const nav = document.getElementById('nav');
+const gears = document.querySelector('.gears');
+const githubProjects = document.querySelector('.github-projects-container');
+
+
+hamburger.onclick = function () {
+    this.classList.toggle('open');
+    nav.classList.toggle('show');
+
+    // Toggle visibility of gears and GitHub projects
+    if (gears.style.visibility === 'hidden') {
+        gears.style.visibility = 'visible';
+        githubProjects.style.visibility = 'visible';
+    } else {
+        gears.style.visibility = 'hidden';
+        githubProjects.style.visibility = 'hidden';
+    }
+};
+
+
+
+function toggleGears() {
+    // Get the elements that have the gear classes
+    var gears = document.querySelectorAll('.gear-rotate, .gear-rotate-left');
+    
+    // Toggle the spinning animation on each gear
+    gears.forEach(function(gear) {
+        if (gear.style.animationPlayState === 'running') {
+            gear.style.animationPlayState = 'paused';
+        } else {
+            gear.style.animationPlayState = 'running';
+        }
+    });
+    
+    // Optionally, toggle the background color
+    document.body.classList.toggle('darken-background');
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetchGithubProjects();
+});
+
+
+
+function fetchGithubProjects() {
+    fetch("https://api.github.com/users/wfgemyd/repos")
+        .then(response => response.json())
+        .then(repos => {
+            // Sort the repositories by the last update date in descending order
+            const sortedRepos = repos.sort((a, b) => new Date(b.pushed_at) - new Date(a.pushed_at));
+            
+            const projectsContainer = document.getElementById("github-projects");
+            if (projectsContainer) {
+                projectsContainer.innerHTML = ''; // Clear existing content
+
+                sortedRepos.forEach(repo => {
+                    fetchLastCommit(repo.commits_url.replace('{/sha}', ''))
+                        .then(lastCommitDate => {
+                            const projectDiv = document.createElement("div");
+                            projectDiv.className = 'project';
+                            projectDiv.innerHTML = `
+                                <div class="project-title"><h3>${repo.name}</h3><span>Last commit: ${lastCommitDate}</span></div>
+                                <div class="project-description"><p>${repo.description || ''}</p></div>
+                                <div class="project-link"><a href="${repo.html_url}" target="_blank">View on GitHub</a></div>
+                            `;
+                            projectsContainer.appendChild(projectDiv);
+                        });
+                });
+            }
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+function fetchLastCommit(commitsUrl) {
+    return fetch(commitsUrl)
+        .then(response => response.json())
+        .then(commits => {
+            if (commits.length > 0) {
+                const lastCommit = commits[0];
+                return new Date(lastCommit.commit.committer.date).toLocaleDateString();
+            } else {
+                return 'No commits';
+            }
+        })
+        .catch(() => 'Unavailable');
+}
+
+
+
+const footer = document.querySelector('footer');
+const nav1 = document.getElementById('nav');
+
+
+function handleScroll() {
+  const footerRect = footer.getBoundingClientRect();
+  const navHeight = nav.offsetHeight;
+  
+  // Check if the footer is coming into the viewport
+  if (window.innerHeight - footerRect.top > 0) {
+    // Adjust the bottom position of the nav based on the footer's position
+    nav1.style.bottom = `${window.innerHeight - footerRect.top}px`;
+  } else {
+    // Reset the bottom position when the footer is not in view
+    nav1.style.bottom = '0';
+  }
+}
+window.addEventListener('scroll', handleScroll);
