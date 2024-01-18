d16aaa74f29676a3a5aa680119f4c69c8f6237dc
wfgemyd <wfgemyd@gmail.com>
2024-01-18 12:29:35 +0100

    new paage and ref was added

---
+++ HobbiesandInterests.js
@@ -0,0 +1,37 @@
+const hamburger = document.getElementById('hamburger');
+const nav = document.getElementById('nav');
+
+
+hamburger.onclick = function () {
+    this.classList.toggle('open');
+    nav.classList.toggle('show');
+
+    // Toggle visibility of gears and GitHub projects
+    if (gears.style.visibility === 'hidden') {
+        gears.style.visibility = 'visible';
+        githubProjects.style.visibility = 'visible';
+    } else {
+        gears.style.visibility = 'hidden';
+        githubProjects.style.visibility = 'hidden';
+    }
+};
+
+
+const footer = document.querySelector('footer');
+const nav1 = document.getElementById('nav');
+
+
+function handleScroll() {
+  const footerRect = footer.getBoundingClientRect();
+  const navHeight = nav.offsetHeight;
+  
+  // Check if the footer is coming into the viewport
+  if (window.innerHeight - footerRect.top > 0) {
+    // Adjust the bottom position of the nav based on the footer's position
+    nav1.style.bottom = `${window.innerHeight - footerRect.top}px`;
+  } else {
+    // Reset the bottom position when the footer is not in view
+    nav1.style.bottom = '0';
+  }
+}
+window.addEventListener('scroll', handleScroll);
